0af512703c49706b4cb62471c780c70c0f4f04d2
Pinn <lsol1130@naver.com>
2025-07-14 21:14:18 +0900

    [💻feat] : 로그인 전 회원가입 및 로그인 화면 UI 구현

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,31 @@
+import CommonButton from "@/components/common/button/CommonButton";
+import { BsChatFill } from "react-icons/bs";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white px-4 py-8 items-center justify-between">
+      
+      {/* Logo Section */}
+      <div className="flex flex-col items-center mt-12">
+        <img
+          src="/PinnBB-Logo.png"
+          alt="Pinnbb Logo"
+          className="w-[clamp(14rem,22vw,18rem)]"
+        />
+      </div>
+
+      {/* Button Section */}
+      <div className="flex flex-col w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg space-y-3 sm:space-y-4 lg:space-y-5 mb-8 sm:mb-12">
+        <CommonButton classNames="flex justify-center items-center bg-yellow-300 hover:bg-yellow-400 cursor-pointer">
+          <BsChatFill className="text-black mr-2"/> 카카오로 로그인
+        </CommonButton>
+        <CommonButton classNames="bg-gray-100 hover:bg-gray-300 cursor-pointer" href="/auth/sign-in">
+          이메일로 로그인
+        </CommonButton>
+        <CommonButton classNames="bg-gray-100 hover:bg-gray-300 cursor-pointer" href="/auth/sign-up">
+          회원가입
+        </CommonButton>
+      </div>
+    </div>
+  )
+}
