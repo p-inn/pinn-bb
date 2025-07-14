07c670ca87a8b45236eb5a412cf0dbe34ff18ef4
Pinn <lsol1130@naver.com>
2025-07-14 21:14:43 +0900

    [➕add] : 임시 UI 작성

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,5 @@
+export default function SignInPage () {
+  return (
+    <div>로그인페이지</div>
+  )
+}
---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,5 @@
+export default function SignUpPage () {
+  return (
+    <div>회원가입페이지</div>
+  )
+}
