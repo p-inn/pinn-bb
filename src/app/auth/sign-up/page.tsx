e9997d134574dc2c8a31a55d5b9b03df54e0dccd
Pinn <lsol1130@naver.com>
2025-07-15 20:56:33 +0900

    [💻feat] : 회원가입 페이지 마크업 구햔

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,5 +1,45 @@
+import BackButton from "@/components/common/button/BackButton";
+import CommonInput from "@/components/common/input/CommonInput";
+import SignInLogoSection from "@/components/common/logo/SignInLogoSection";
+
+
 export default function SignUpPage () {
   return (
-    <div>회원가입페이지</div>
+    <div className="w-full max-w-[560px] mx-auto">
+      <div className="flex justify-start px-4">
+        <BackButton />
+      </div>
+      <SignInLogoSection />
+      <form className="mt-8">
+        <div className="flex flex-col items-center justify-center">
+          <CommonInput
+            classNames="bg-gray-100 border-none"
+            type="text"
+            placeholder="아이디를 입력해주세용!"
+          />
+          <CommonInput
+            classNames="bg-gray-100 border-none"
+            type="password"
+            placeholder="패스워드를 입력해주세용!"
+          />
+          <CommonInput
+            classNames="bg-gray-100 border-none"
+            type="password"
+            placeholder="패스워드를 다시 한번 입력해주세요!"
+          />
+          <CommonInput
+            classNames="bg-gray-100 border-none"
+            type="text"
+            placeholder="사용하실 닉네임을 입력해주세용!"
+          />
+          <CommonInput
+            classNames="bg-gray-100 border-none"
+            type="tel"
+            placeholder="휴대폰번호를 입력해주세용!"
+          />
+        </div>
+        
+      </form>
+    </div>
   )
 }
