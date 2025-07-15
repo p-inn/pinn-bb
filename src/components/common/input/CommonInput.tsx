bfa113cff54947a48f86cfeecbab6bba81e5fc41
Pinn <lsol1130@naver.com>
2025-07-15 23:11:50 +0900

    [🎀style]: 반응형 CSS 일부 수정

--- src/components/common/input/CommonInput.tsx
+++ src/components/common/input/CommonInput.tsx
@@ -2,7 +2,7 @@ import { cva } from 'class-variance-authority';
 import { CommonInputProps } from '@/types/components';
 import { cn } from '@/utils/cn';
 
-const CommonInputStyle = cva('pl-3 py-3 rounded-md w-full focus:outline-none  max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg space-y-3 sm:space-y-4 lg:space-y-5 mb-6', {
+const CommonInputStyle = cva('pl-3 py-3 rounded-md w-full focus:outline-none mb-6', {
   variants: {
     variant: {
       default: 'bg-white border border-solid border-border-300',
