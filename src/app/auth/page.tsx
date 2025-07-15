7c71eba494887a3347dda657453e984a6859fe06
Pinn <lsol1130@naver.com>
2025-07-15 20:54:37 +0900

    [🕹️refactor] : SignLogoSection 컴포넌트 공통으로 분기

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,19 +1,12 @@
 import CommonButton from "@/components/common/button/CommonButton";
+import SignInLogoSection from "@/components/common/logo/SignInLogoSection";
 import { BsChatFill } from "react-icons/bs";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col min-h-screen bg-white px-4 py-8 items-center justify-between">
+    <div className="flex flex-col min-h-screen bg-white mx-auto px-4 py-8 items-center justify-between">
+      <SignInLogoSection />
       
-      {/* Logo Section */}
-      <div className="flex flex-col items-center mt-12">
-        <img
-          src="/PinnBB-Logo.png"
-          alt="Pinnbb Logo"
-          className="w-[clamp(14rem,22vw,18rem)]"
-        />
-      </div>
-
       {/* Button Section */}
       <div className="flex flex-col w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg space-y-3 sm:space-y-4 lg:space-y-5 mb-8 sm:mb-12">
         <CommonButton classNames="flex justify-center items-center bg-yellow-300 hover:bg-yellow-400 cursor-pointer">
