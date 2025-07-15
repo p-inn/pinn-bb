ad42817adf20c2e0d9b4c5f8df5b5e00c123706d
Pinn <lsol1130@naver.com>
2025-07-15 20:56:44 +0900

    [💻feat]: 로그인 페이지 마크업 구현

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,5 +1,26 @@
+import BackButton from "@/components/common/button/BackButton";
+import CommonInput from "@/components/common/input/CommonInput";
+import SignInLogoSection from "@/components/common/logo/SignInLogoSection";
+
 export default function SignInPage () {
   return (
-    <div>로그인페이지</div>
+    <div className="w-full max-w-[560px] mx-auto">
+      <div className="flex justify-start px-4">
+        <BackButton />
+      </div>
+      <SignInLogoSection />
+      <div className="flex flex-col items-center justify-center">
+          <CommonInput
+            classNames="bg-gray-100 border-none"
+            type="text"
+            placeholder="아이디를 입력해주세용!"
+          />
+          <CommonInput
+            classNames="bg-gray-100 border-none"
+            type="password"
+            placeholder="패스워드를 입력해주세용!"
+          />
+        </div>
+    </div>
   )
 }
