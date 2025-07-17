e9bc63de89122ff8de334bfac535ff75b6fc6da6
Pinn <lsol1130@naver.com>
2025-07-18 00:45:28 +0900

    [💻feat]: 전체 페이지에 공통으로 들어가는 header 마크업

---
+++ src/components/feat/header/Header.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+// import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { FaChartColumn, FaArrowRightFromBracket, FaList } from "react-icons/fa6";
+
+export default function Header() {
+
+  return (
+    <div className="fixed inset-x-0 top-0 w-full z-50">
+      <div className="flex items-center bg-mainColor-500/35 justify-between h-20 px-3 sm:px-6">
+        <div className="hidden sm:block w-20" />
+        <div className="flex-1 flex sm:justify-center justify-start min-w-[120px] sm:min-w-[180px]">
+          <img
+            src="/PinnBB-Logo.png"
+            alt="PinnBB Logo"
+            className="w-[120px] sm:w-[180px] h-auto mr-2 sm:mr-0"
+          />
+        </div>
+        <div className=" flex gap-2 sm:gap-5 text-mainColor-500 text-xl sm:text-2xl cursor-pointer mt-8">
+          <FaChartColumn />
+          <FaList />
+          <FaArrowRightFromBracket />
+        </div>
+      </div>
+    </div>
+  );
+}
