6adaaf5f654f71dfd8b0fefa1101264fe19952e6
Pinn <lsol1130@naver.com>
2025-07-24 21:01:18 +0900

    [💻feat] : 월별 수입/지출 chart 마크업
    - 임의 데이터로 하드코딩 진행
    - 추후 실제 데이터 연동 및 수정 예정

--- src/app/chart-page/page.tsx
+++ src/app/chart-page/page.tsx
@@ -1,5 +1,6 @@
 import Divider from "@/components/common/divider/CommonDivider";
 import DonutChart from "@/components/feat/chart/DonutChart";
+import PayChart from "@/components/feat/chart/PayChart";
 import Header from "@/components/feat/header/CommonHeader";
 import MonthlyHeader from "@/components/feat/header/MonthlyHeader";
 
@@ -13,6 +14,9 @@ export default function ChartPage () {
       <div className="flex flex-col justify-center w-full sm:w-3/4 mx-auto">
         <Divider title="카테고리별 지출">
           <DonutChart />
+        </Divider>
+        <Divider title="월별 수입/지출">
+        <PayChart />
         </Divider>
       </div>
     </div>
