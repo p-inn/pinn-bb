1021655cb83c1b2b5b57f19b40bbe9bb184a2fe8
Pinn <lsol1130@naver.com>
2025-07-25 21:06:23 +0900

    [🎀style] : 하단 선 제거 (미관상 불필요하다고 판단)

--- src/components/feat/budget-list/DailyBudget.tsx
+++ src/components/feat/budget-list/DailyBudget.tsx
@@ -1,8 +1,8 @@
 import CategoryBadge from "../CategoryBadge";
 
-export default function BudgetList() {
+export default function BudgetItem() {
   return (
-    <div className="flex justify-between items-center pb-4 border-b border-b-gray-300 min-w-0 text-sm sm:text-base my-4 text-gray-500">
+    <div className="flex justify-between items-center pb-4 min-w-0 text-sm sm:text-base my-4 text-gray-500">
       <div className="flex justify-start items-center gap-2">
         <span>2025.08.12</span>
         {/* category-badge */}
