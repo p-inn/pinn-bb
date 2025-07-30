66a8d3966059f361b6edfa031e03cb923ff9925e
Pinn <lsol1130@naver.com>
2025-07-30 23:21:33 +0900

    [💻feat] : 회원가입에 필요한 type 추가

--- src/types/components.ts
+++ src/types/components.ts
@@ -12,11 +12,23 @@ export interface ButtonProps
   variant?: 'default';
 }
 
-export type InputType = 'text' | 'password' | 'email' | 'date' | 'number' | 'tel';
+export type InputType = 'text' | 'password' | 'email' | 'date' | 'number' | 'tel' | 'name' | 'passwordCheck';
 export interface CommonInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   type: InputType;
   variant?: 'default' | 'disabled';
   classNames?: string;
   label?: string;
+}
+
+export interface SignInData {
+  email: string;
+  password: string;
+}
+
+export interface SignUpData extends SignInData {
+  name?: string;
+  passwordCheck?: string;
+  nickName?: string;
+  tel?: string;
 }
