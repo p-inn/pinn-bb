e736589fa8a101712bdaf0f5b0818eafc604d224
Pinn <lsol1130@naver.com>
2025-07-30 23:24:54 +0900

    [💻feat] : sonner Toaster layout에 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from 'next/font/local'
 import "./globals.css";
 import ReactQueryProvider from "@/provider/ReactQueryProvider";
+import { Toaster } from 'sonner';
 
 const pretendard = localFont({
   src: '../fonts/pretendard/PretendardVariable.woff2',
@@ -29,6 +30,12 @@ export default function RootLayout({
         <ReactQueryProvider>
             <main className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-6 min-w-72">
               {children}
+              <Toaster
+                position="top-right"
+                richColors
+                duration={2500}
+                expand
+              />
             </main>
         </ReactQueryProvider>
       </body>
