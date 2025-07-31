3584486c2b5cb370b005a386c2f8a49207af0322
Pinn <lsol1130@naver.com>
2025-07-31 21:27:00 +0900

    [🕹️refactor]  : auth 화면 내 버튼 분기 후 로그인 훅 추가

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,23 +1,11 @@
-import CommonButton from "@/components/common/button/CommonButton";
 import SignInLogoSection from "@/components/common/logo/SignInLogoSection";
+import SignInSection from "@/components/feat/auth/SignInSection";
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col min-h-screen bg-white mx-auto px-4 py-8 items-center justify-between">
       <SignInLogoSection />
-      
-      {/* Button Section */}
-      <div className="flex flex-col w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg space-y-3 sm:space-y-4 lg:space-y-5 mb-8 sm:mb-12">
-        <CommonButton classNames="flex justify-center items-center hover:bg-gray-100 border cursor-pointer">
-          <img className="w-6 h-6 mr-2" src="/google-icon.png"/> 구글 로그인
-        </CommonButton>
-        <CommonButton classNames="bg-gray-100 hover:bg-gray-200 cursor-pointer" href="/auth/sign-in">
-          이메일로 로그인
-        </CommonButton>
-        <CommonButton classNames="bg-gray-100 hover:bg-gray-200 cursor-pointer" href="/auth/sign-up">
-          회원가입
-        </CommonButton>
-      </div>
+      <SignInSection />
     </div>
   )
 }
