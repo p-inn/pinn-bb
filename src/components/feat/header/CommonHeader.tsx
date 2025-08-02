67d0be94d156968075c81aba98ed1fc08425b485
Pinn <lsol1130@naver.com>
2025-08-02 22:01:38 +0900

    [💻feat] 구현한 AddBudgetModal header에 추가
    - 전역적으로 상태 관리 하기 위한 useModalStore 사용
    - header에서만 해당 모달이 열리게 하기 위함

--- src/components/feat/header/CommonHeader.tsx
+++ src/components/feat/header/CommonHeader.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useSignOut } from "@/hooks/useSign";
+import { useModalStore } from "@/store/modalStore";
 import Link from "next/link";
-import { FaChartColumn, FaArrowRightFromBracket, FaList } from "react-icons/fa6";
+import { FaChartColumn, FaArrowRightFromBracket, FaList, FaRegSquarePlus } from "react-icons/fa6";
+import AddBudgetModal from "../budget-list/AddBudgetModal";
 
 export default function Header() {
+  const { openAddBudgetModal } = useModalStore();
   const { mutate: signOut } = useSignOut();
   const icons = [
   { icon: <FaChartColumn />, href: "/chart-page" },
@@ -12,25 +15,29 @@ export default function Header() {
 ];
 
   return (
-    <div className="fixed inset-x-0 top-0 w-full z-50">
-      <div className="flex items-center bg-mainColor-300 justify-between h-20 px-3 sm:px-6">
-        <div className="hidden sm:block w-20" />
-        <div className="flex-1 flex sm:justify-center justify-start min-w-[120px] sm:min-w-[180px]">
-          <img
-            src="/PinnBB-Logo.png"
-            alt="PinnBB Logo"
-            className="w-[120px] sm:w-[180px] h-auto mr-2 sm:mr-0"
-          />
-        </div>
-        <div className=" flex gap-2 sm:gap-5 text-mainColor-500 text-xl sm:text-2xl cursor-pointer mt-8">
-          {icons.map(({ icon, href }, idx) => (
-            <Link key={href} href={href}>
-              {icon}
-            </Link>
-          ))}
-          <FaArrowRightFromBracket onClick={() => signOut()}/> 
+    <>
+      <div className="fixed inset-x-0 top-0 w-full">
+        <div className="flex items-center bg-mainColor-300 justify-between h-20 px-3 sm:px-6">
+          <div className="hidden sm:block w-20" />
+          <div className="flex-1 flex sm:justify-center justify-start min-w-[120px] sm:min-w-[180px]">
+            <img
+              src="/PinnBB-Logo.png"
+              alt="PinnBB Logo"
+              className="w-[120px] sm:w-[180px] h-auto mr-2 sm:mr-0"
+            />
+          </div>
+          <div className="flex gap-2 sm:gap-5 text-mainColor-500 text-xl sm:text-2xl cursor-pointer mt-8">
+            <FaRegSquarePlus onClick={openAddBudgetModal} className="hover:text-mainColor-600" />
+            {icons.map(({ icon, href }) => (
+              <Link key={href} href={href}>
+                {icon}
+              </Link>
+            ))}
+            <FaArrowRightFromBracket onClick={() => signOut()} />
+          </div>
         </div>
       </div>
-    </div>
+      <AddBudgetModal />
+    </>
   );
 }
