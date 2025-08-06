180f90fdef3933c0bde4a71fc4079652f62760a1
Pinn <lsol1130@naver.com>
2025-08-06 22:07:06 +0900

    [💻feat] : supabase API 구현

--- src/api/budget.ts
+++ src/api/budget.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/supabase/supabaseClient";
 import { BudgetListData } from "@/types/form";
+import dayjs from "dayjs";
 
 export const addBudget = async (newBudget: Omit<BudgetListData, "id">) => {
   const { data, error } = await supabase
@@ -10,3 +11,15 @@ export const addBudget = async (newBudget: Omit<BudgetListData, "id">) => {
   if (error) throw error;
   return data?.[0];
 };
+
+export async function fetchBudgetsByMonth(ym: string, userId: string) {
+  const { data, error } = await supabase
+    .from("budgets")
+    .select("*")
+    .eq("user_id", userId)
+    .gte("date", `${ym}-01`)
+    .lt("date", dayjs(ym).add(1, "month").format("YYYY-MM-01"));
+
+  if (error) throw error;
+  return data;
+}
