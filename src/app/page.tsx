59be72cfa5c4de849cf97e2c6bfda0f3a3f8e174
Pinn <lsol1130@naver.com>
2025-08-07 22:00:28 +0900

    [🔥hotfix] : 외부 디바이스 접근 시 인증 관련 오류 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import HomeAuthPage from "@/components/feat/auth/HomeAuth";
+import { redirect } from 'next/navigation';
+import { createSupabaseServerClient } from "@/supabase/supabaseServer";
+import MainPage from "./dashboard-page/page";
 
-export default function Home() {
-  return (
-    <div>
-      <HomeAuthPage />
-    </div>
-  );
+export default async function Home() {
+    const supabase = createSupabaseServerClient();
+  const { data: { session } } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect('/auth');
+  }
+  return <MainPage />;
 }
--- src/components/feat/auth/HomeAuth.tsx
+++ src/components/feat/auth/HomeAuth.tsx
@@ -1,12 +1,12 @@
-"use client";
+// "use client";
 
-import LoginPage from "@/app/auth/page";
-import MainPage from "@/app/dashboard-page/page";
-import { useAuthStore } from "@/store/authStore";
+// import LoginPage from "@/app/auth/page";
+// import MainPage from "@/app/dashboard-page/page";
+// import { useAuthStore } from "@/store/authStore";
 
 
-export default function HomeAuthPage() {
-  const isLoggedIn = useAuthStore(s => s.isLoggedIn);
+// export default function HomeAuthPage() {
+//   const isLoggedIn = useAuthStore(s => s.isLoggedIn);
 
-  return isLoggedIn ? <MainPage /> : <LoginPage />;
-}
+//   return isLoggedIn ? <MainPage /> : <LoginPage />;
+// }
