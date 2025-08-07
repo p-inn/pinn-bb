628a1e5f846c964c639f26d7bfa0c1e1d3f9e6eb
Pinn <lsol1130@naver.com>
2025-08-07 17:37:10 +0900

    [🕹️refactor] : server/client 분리를 위한 컴포넌트 분기

--- src/app/chart-page/page.tsx
+++ src/app/chart-page/page.tsx
@@ -1,20 +1,11 @@
-import Divider from "@/components/common/divider/CommonDivider";
-import DonutChart from "@/components/feat/chart/DonutChart";
-import PayChart from "@/components/feat/chart/PayChart";
+import ChartDashBoard from "@/components/feat/chart/ChartDashBoard";
 import Header from "@/components/feat/header/CommonHeader";
 
 export default function ChartPage () {
   return (
     <div>
       <Header />
-      <div className="flex flex-col justify-center w-full sm:w-3/4 mx-auto">
-        <Divider title="카테고리별 지출">
-          <DonutChart />
-        </Divider>
-        <Divider title="월별 수입/지출">
-        <PayChart />
-        </Divider>
-      </div>
+      <ChartDashBoard />
     </div>
   )
 }
